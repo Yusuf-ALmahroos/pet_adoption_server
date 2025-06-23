6a8174d3576322c50936027348e1bd9a694323b1
thaistrok <thaistrok@gmail.com>
2025-06-23 15:50:21 +0300

    models

---
+++ models/pet.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const petSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  type: { type: String, required: true },
+  breed: { type: String, required: true },
+  age: { type: Number, required: true },
+  description: { type: String },
+  shelterId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+});
+
+module.exports = mongoose.model('Pet', petSchema); 
