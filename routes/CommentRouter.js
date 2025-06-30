14e2d6187bcfd6dd5f2b99ae248faad905f96256
Yousif Shukri <yousif4shukri@gmail.com>
2025-06-30 21:18:42 +0300

    fixed comments

--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -3,7 +3,7 @@ const commentController = require('../controllers/CommentController');
 const middleware = require('../middleware');
 
 router.post('/', middleware.stripToken, middleware.verifyToken, commentController.CreateComment);
-router.get('pet/:petId', commentController.GetPetComment);
+router.get('/pet/:petId', commentController.GetPetComment);
 router.put('/:commentId', middleware.stripToken, middleware.verifyToken, commentController.DeleteComment);
 
 module.exports = router;
